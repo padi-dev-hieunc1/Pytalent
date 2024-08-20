55738e9e0b2ec32627e4ad729cd6cfa9b9539d5c
padi-dev-hieunc1 <hieunc1@paditech.com>
2024-08-20 10:54:16 +0700

    divide function

--- src/modules/results/services/logical-answer.service.ts
+++ src/modules/results/services/logical-answer.service.ts
@@ -11,7 +11,6 @@ import { AnswerStatusEnum } from '@common/enum/answer-status.enum';
 import { LogicalAnswersRepository } from '../repositories/logical-answer.repository';
 import { LogicalQuestionsRepository } from '@modules/logical-questions/repositories/logical-question.repository';
 import { GameResultsService } from './result.service';
-import { GameResultStatusEnum } from '@common/enum/game-result-status.enum';
 
 @Injectable()
 export class LogicalAnswersService {
@@ -40,30 +39,67 @@ export class LogicalAnswersService {
     return initialLogicalAnswer;
   }
 
-  async answerLogicalQuestion(
+  async validateLogicalAnswer(
     resultId: number,
     questionId: number,
     params: AnswerLogicalQuestionInterface,
   ) {
-    const [result, logicalAnswer, question] = await Promise.all([
-      this.gameResultService.getDetailGameResult(resultId),
-      this.logicalAnswerRepository.findOne({
-        where: {
-          questionId: questionId,
-          resultId: resultId,
-        },
-      }),
-      this.logicalQuestionRepository.findOne({
-        where: {
-          id: questionId,
-        },
-      }),
-    ]);
+    const logicalQuestion = await this.getLogicalQuestion(questionId);
+    const initialLogicalAnswer = await this.getLogicalAnswer(
+      questionId,
+      resultId,
+    );
 
-    if (!question)
+    const checkCorrect =
+      params.candidate_answer === logicalQuestion.result ? 1 : 0;
+    const answerStatus = params.candidate_answer
+      ? AnswerStatusEnum.DONE
+      : AnswerStatusEnum.SKIP;
+
+    const paramUpdate = plainToClass(LogicalAnswers, {
+      candidateAnswer: params.candidate_answer,
+      isCorrect: checkCorrect,
+      status: answerStatus,
+    });
+
+    await this.logicalAnswerRepository.update(
+      initialLogicalAnswer.id,
+      paramUpdate,
+    );
+
+    if (checkCorrect) {
+      return {
+        checkResult: true,
+      };
+    }
+
+    return {
+      checkResult: false,
+    };
+  }
+
+  async getLogicalQuestion(questionId: number) {
+    const logicalQuestion = await this.logicalQuestionRepository.findOne({
+      where: {
+        id: questionId,
+      },
+    });
+
+    if (!logicalQuestion)
       throw new CustomizeException(
         this.i18n.t('message.LOGICAL_QUESTION_NOT_FOUND'),
       );
+
+    return logicalQuestion;
+  }
+
+  async getLogicalAnswer(questionId: number, resultId: number) {
+    const logicalAnswer = await this.logicalAnswerRepository.findOne({
+      where: {
+        questionId: questionId,
+        resultId: resultId,
+      },
+    });
 
     if (!logicalAnswer)
       throw new CustomizeException(
@@ -73,40 +109,6 @@ export class LogicalAnswersService {
     if (logicalAnswer.candidateAnswer)
       throw new CustomizeException(this.i18n.t('message.LOGICAL_ANSWER_DONE'));
 
-    if (
-      result.completeTime <= 90 &&
-      result.status === GameResultStatusEnum.NOT_COMPLETED
-    ) {
-      const check_correct = params.candidate_answer === question.result ? 1 : 0;
-      const answer_status = params.candidate_answer
-        ? AnswerStatusEnum.DONE
-        : AnswerStatusEnum.SKIP;
-
-      const paramUpdate = plainToClass(LogicalAnswers, {
-        candidate_answer: params.candidate_answer,
-        is_correct: check_correct,
-        status: answer_status,
-      });
-
-      await this.logicalAnswerRepository.update(logicalAnswer.id, paramUpdate);
-
-      return paramUpdate;
-    }
-    return null;
-  }
-
-  async getDetailLogicalAnswer(resultId: number, questionId: number) {
-    const logicalAnswer = await this.logicalAnswerRepository.findOne({
-      where: {
-        resultId: resultId,
-        questionId: questionId,
-      },
-    });
-
-    if (logicalAnswer) return logicalAnswer;
-    else
-      throw new CustomizeException(
-        this.i18n.t('message.LOGICAL_ANSWER_NOT_FOUND'),
-      );
+    return logicalAnswer;
   }
 }
